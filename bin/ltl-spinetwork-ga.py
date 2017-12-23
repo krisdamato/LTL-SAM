08ef74a2e64329fd433cd44d80b1f6866185e2a5
krisdamato <kristian.damato@gmail.com>
2017-12-23 11:02:07 +0100

    Changed simulation parameters.

--- bin/ltl-spinetwork-ga.py
+++ bin/ltl-spinetwork-ga.py
@@ -57,8 +57,8 @@ def main():
     optimizee = SPINetworkOptimizee(traj, n_NEST_threads=1, time_resolution=0.1, plots_directory=paths.output_dir_path, num_fitness_trials=1)
 
     # NOTE: Outerloop optimizer initialization
-    parameters = GeneticAlgorithmParameters(seed=0, popsize=96, CXPB=0.5,
-                                            MUTPB=1.0, NGEN=40, indpb=0.05,
+    parameters = GeneticAlgorithmParameters(seed=0, popsize=200, CXPB=0.5,
+                                            MUTPB=1.0, NGEN=50, indpb=0.0,
                                             tournsize=20, matepar=0.5,
                                             mutpar=1.0, remutate=False
                                             )
