235fd051f48daa4ebf7f3e6392441091cd17ca9a
Kristian <kristian.damato@gmail.com>
2018-06-18 14:28:05 +0000

    Renamed script file.

---
+++ run_baseline_samgraph.sh
@@ -0,0 +1,13 @@
+source ../NEST/bin/nest_vars.sh
+
+#python -m scoop -n 96 bin/ltl-samgraph-ga.py -n SAMGRAPH-0_1ms-GA-Pecevski-handling_first -r 0.1 -fd 0.1 -p -s first
+#python -m scoop -n 96 bin/ltl-samgraph-ga.py -n SAMGRAPH-0_2ms-GA-Pecevski-handling_first -r 0.1 -fd 0.2 -p -s first
+#python -m scoop -n 96 bin/ltl-samgraph-ga.py -n SAMGRAPH-0_5ms-GA-Pecevski-handling_first -r 0.1 -fd 0.5 -p -s first
+#python -m scoop -n 96 bin/ltl-samgraph-ga.py -n SAMGRAPH-1_0ms-GA-Pecevski-handling_first -r 0.1 -fd 1.0 -p -s first
+#python -m scoop -n 96 bin/ltl-samgraph-ga.py -n SAMGRAPH-2_0ms-GA-Pecevski-handling_first -r 0.1 -fd 2.0 -p -s first
+
+python -m scoop -n 96 bin/ltl-samgraph-nes.py -n SAMGRAPH-0_1ms-GA-Pecevski-handling_first -r 0.1 -fd 0.1 -p -s first
+python -m scoop -n 96 bin/ltl-samgraph-nes.py -n SAMGRAPH-0_2ms-GA-Pecevski-handling_first -r 0.1 -fd 0.2 -p -s first
+python -m scoop -n 96 bin/ltl-samgraph-nes.py -n SAMGRAPH-0_5ms-GA-Pecevski-handling_first -r 0.1 -fd 0.5 -p -s first
+python -m scoop -n 96 bin/ltl-samgraph-nes.py -n SAMGRAPH-1_0ms-GA-Pecevski-handling_first -r 0.1 -fd 1.0 -p -s first
+python -m scoop -n 96 bin/ltl-samgraph-nes.py -n SAMGRAPH-2_0ms-GA-Pecevski-handling_first -r 0.1 -fd 2.0 -p -s first
